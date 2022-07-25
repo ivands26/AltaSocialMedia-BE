7a8e623923efc34a7539ccd64190194f55d59647
ivands26 <ivandwis.fis12@gmail.com>
2022-07-25 11:46:03 +0700

    model user

--- domain/user.go
+++ domain/user.go
@@ -3,6 +3,8 @@ package domain
 type User struct {
 	ID       int
 	Nama     string
+	Username string
 	Email    string
 	Password string
+	No_HP    string
 }
--- feature/user/data/model.go
+++ feature/user/data/model.go
@@ -1,10 +1,47 @@
 package data
 
-import "gorm.io/gorm"
+import (
+	"github.com/AltaProject/AltaSocialMedia/domain"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	ID    int
-	Nama  string
-	Email string
+	ID       int    `json:"id" form:"id" gorm:"primaryKey:autoIncrement"`
+	Nama     string `json:"nama" form:"nama" validate:"required"`
+	Username string `json:"username" form:"username"`
+	Email    string `json:"email" form:"email" validate:"required"`
+	Password string `json:"password" form:"password" validate:"required"`
+	No_HP    string `json:"no_hp" form:"no_hp"`
+}
+
+func (u *User) ToModel() domain.User {
+	return domain.User{
+		ID:       u.ID,
+		Nama:     u.Nama,
+		Username: u.Username,
+		Email:    u.Email,
+		Password: u.Password,
+		No_HP:    u.No_HP,
+	}
+}
+
+func ParseToArr(arr []User) []domain.User {
+	var res []domain.User
+
+	for _, val := range arr {
+		res = append(res, val.ToModel())
+	}
+	return res
+}
+
+func FromModel(data domain.User) User {
+	var res User
+	res.ID = data.ID
+	res.Nama = data.Nama
+	res.Username = data.Username
+	res.Email = data.Email
+	res.Password = data.Password
+	res.No_HP = data.No_HP
+	return res
 }
