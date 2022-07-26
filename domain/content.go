dff8004dbf49cef4b0351109ea55adc9a465d8b5
Rizki Firdaus <rizki.achanation@gmail.com>
2022-07-26 11:47:42 +0700

    add feature content post getID

---
+++ domain/content.go
@@ -0,0 +1,25 @@
+package domain
+
+import "github.com/labstack/echo/v4"
+
+type Content struct {
+	ID      int
+	Content string
+	UserID  int
+	// Description string
+}
+
+type ContentHandler interface {
+	PostContent() echo.HandlerFunc
+	GetSpecificContent() echo.HandlerFunc
+}
+
+type ContentUseCases interface {
+	Posting(newContent Content) (Content, error)
+	GetContentId(contentId int) (Content, error)
+}
+
+type ContentData interface {
+	Posting(newContent Content) (Content, error)
+	GetContentId(contentId int) (Content, error)
+}
--- feature/content/data/model.go
+++ feature/content/data/model.go
@@ -1,10 +1,35 @@
 package data
 
-import "gorm.io/gorm"
+import "github.com/AltaProject/AltaSocialMedia/domain"
 
-type User struct {
-	gorm.Model
-	ID    int
-	Nama  string
-	Email string
+type Content struct {
+	ID      int    `json:"id" form:"id" gorm:"primaryKey:autoIncrement"`
+	Content string `json:"nama" form:"nama" validate:"required"`
+	// Description string `json:"username" form:"username"`
+	UserID int
+}
+
+func (content *Content) toDomainContent() domain.Content {
+	return domain.Content{
+		ID:      int(content.ID),
+		Content: content.Content,
+		UserID:  content.UserID,
+	}
+}
+
+func ParseArrDomainContent(arr []Content) []domain.Content {
+	var res []domain.Content
+
+	for _, val := range arr {
+		res = append(res, val.toDomainContent())
+	}
+	return res
+}
+
+func ToLocalContent(data domain.Content) Content {
+	var res Content
+	res.ID = int(data.ID)
+	res.Content = data.Content
+	res.UserID = data.UserID
+	return res
 }
