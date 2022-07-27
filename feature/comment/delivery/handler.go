ae4d5051d9a04826ab996f041486803ffe99f14a
Rizki Firdaus <rizki.achanation@gmail.com>
2022-07-27 20:19:01 +0700

    add comment deliver, usecase, deliver

--- feature/comment/delivery/handler.go
+++ feature/comment/delivery/handler.go
@@ -1,1 +1,40 @@
 package delivery
+
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/AltaProject/AltaSocialMedia/domain"
+	"github.com/labstack/echo/v4"
+)
+
+type commentHandler struct {
+	commentCases domain.CommentUseCases
+}
+
+func New(cs domain.CommentUseCases) domain.CommentHandler {
+	return &commentHandler{
+		commentCases: cs,
+	}
+}
+
+func (cs *commentHandler) GetAllComment() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		data, err := cs.commentCases.GetAllComment()
+		if err != nil {
+			if strings.Contains(err.Error(), "not found") {
+				log.Println("User Handler", err)
+				c.JSON(http.StatusNotFound, err.Error())
+			} else if strings.Contains(err.Error(), "retrieve") {
+				log.Println("User Handler", err)
+				c.JSON(http.StatusInternalServerError, err.Error())
+			}
+		}
+
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "success get all user data",
+			"data":    data,
+		})
+	}
+}
--- feature/comment/delivery/route.go
+++ feature/comment/delivery/route.go
@@ -1,1 +1,14 @@
 package delivery
+
+import (
+	"github.com/AltaProject/AltaSocialMedia/config"
+	"github.com/AltaProject/AltaSocialMedia/domain"
+	"github.com/AltaProject/AltaSocialMedia/feature/content/delivery/middlewares"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func RouteContent(e *echo.Echo, ch domain.CommentHandler) {
+	e.GET("/content/:id", ch.GetAllComment(), middleware.JWTWithConfig(middlewares.UseJWT([]byte(config.SECRET))))
+
+}
