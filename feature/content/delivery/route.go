a299a8b3d93e8d5024910fbe8a6736569a48281b
Rizki Firdaus <rizki.achanation@gmail.com>
2022-07-27 21:41:39 +0700

    fixed 2

--- feature/content/delivery/route.go
+++ feature/content/delivery/route.go
@@ -13,5 +13,6 @@ func RouteContent(e *echo.Echo, ch domain.ContentHandler) {
 	e.POST("/content/:id", ch.Update(), middleware.JWTWithConfig(middlewares.UseJWT([]byte(config.SECRET))))
 	e.DELETE("/content/:id", ch.Delete(), middleware.JWTWithConfig(middlewares.UseJWT([]byte(config.SECRET))))
 	e.GET("/content/:id", ch.GetSpecificContent(), middleware.JWTWithConfig(middlewares.UseJWT([]byte(config.SECRET))))
+	e.GET("/content", ch.GetSpecificContent())
 
 }
